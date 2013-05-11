70f3ef8980e554056a40e596b30a6fb4078eac08
walnutown <mymovoo@gmail.com>
2013-05-11 17:20:00 +0800

    acknowledgements

--- green.py
+++ green.py
@@ -1,6 +1,73 @@
+# _*_ coding: utf-8 _*_
+
 import datetime
 import os
 import random
+from random import choice
+
+comments = [
+    'Update ReactFiberScheduler',
+    'Update CHANGELOG for 16.7',
+    'v16.7.0',
+    'Move SchedulerFeatureFlags fork to src directory to fix lint',
+    'Make scheduler debugging feature flag static',
+    'Create separate SchedulerFeatureFlags instead of using ReactFeatureFl…',
+    'Memoize promise listeners to prevent exponential growth',
+    'Removed Fabric-specific feature flag files and updated Rollup to use',
+    'Inject overrideProps() fn to DevTools',
+    'Add support for Suspense & lazy() to the react-is package',
+    'Updated version incrementing suggestion in release script based on te…',
+    'Implement pauseExecution, continueExecution, dumpQueue for Scheduler',
+    'Added ErrorBoundary tests for useEffect and useLayoutEffect',
+    'Enable hooks in fabric',
+    'Tweaked wording for v8 "performance cliff" issue',
+    'Fix scheduler setTimeout() re-entrancy check ',
+    'Prevent a v8 deopt when profiling (#14383)',
+    'Automated fixture tests',
+    'Removed unnecessary externals from Jest bundles',
+    'Fixed scheduler setTimeout fallback',
+    'Fix bug in cloneHook',
+    '[Fire] Add initial build infrastructure',
+    'Validate propTypes for lazy() and memo() and warn about invalid patte…',
+    'Adding isMemo check to react-is package',
+    'Remove useMutationEffect',
+    'Add a null type test for memo',
+    'Make useEffect(async) warning more verbose ',
+    'Minified Deep Learn  ',
+    'Deep net learn',
+    'update credits',
+    'update url',
+    'Update rnnrbm.py',
+    'Update segmentation tutorials and .gitignore.',
+    'Preload data for 1D cortical dataset',
+    'Results for unet',
+    'added per class jaccard',
+    'Formatting',
+    'Remove files that are not used any more',
+    'More small fixes',
+    'data augmentation fixed',
+    'remove loading pretrained weights',
+    'index updated with new links',
+    'removed files .pyc',
+    'small change',
+    'import conv2DDDNlayer',
+    'fix input dim and details',
+    'fix dataset loader for em, name for polyps',
+    'adde training loop link',
+    'small changes',
+    'relative paths',
+    'with BN',
+    'update from pascal comment',
+    'polyps results image',
+    'acknowledgements',
+    'added code',
+    'first commit for fcn1D segmentations',
+    'deleted files',
+    'figure 4 description',
+    'explanation ground truth vs predicted segmentation',
+    'FCN description fixed',
+    'cortical layers imagse'
+]
 
 def modify():
     file = open('zero.md', 'r')
@@ -15,7 +82,7 @@ def modify():
 
 
 def commit():
-    os.system('git commit -a -m test_github')
+    os.system('git commit -a -m ' + choice(comments))
 
 
 def set_sys_time(day, month, year):
